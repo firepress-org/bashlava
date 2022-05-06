86d27e377793a62b7cd3462c3e687ace805a9172
Pascal Andy <pascalandy@users.noreply.github.com>
2022-05-06 09:35:57 -0400

    Refactoring edge(), a cleaner way to handle if a branch exist or not

--- bashlava_config.sh
+++ bashlava_config.sh
@@ -1,15 +1,15 @@
 # wip brainstrom
 
-
 export APP_NAME="app_no_name"
 export APP_VERSION="0.1.0"
-
 
 export USING_PRIVATE_SCRIPTS="false"
 
 export USER_IS="${USER}"
-export LOG_LINE_NUMBERS="8"
+
 
 export DEFAULT_BRANCH="main"
 export DEV_BRANCH="edge_${USER_IS}"
 
+# CMD CONFIGS
+export LOG__LINE_NUMBERS="8" #default is 8
