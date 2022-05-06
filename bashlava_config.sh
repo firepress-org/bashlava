260eddc747008fcd46be6bfdc2c9e9ef03027c00
Pascal Andy <pascalandy@users.noreply.github.com>
2022-05-05 20:28:51 -0400

    Refactoring alias.sh

--- bashlava_config.sh
+++ bashlava_config.sh
@@ -1,9 +1,15 @@
 # wip brainstrom
 
 
-export APP_NAME="bashlava"
-export APP_VERSION="9.9.9"
+export APP_NAME="app_no_name"
+export APP_VERSION="0.1.0"
 
-export PRIVATE_scripts="false"
+
+export USING_PRIVATE_SCRIPTS="false"
+
 export USER_IS="${USER}"
 export LOG_LINE_NUMBERS="8"
+
+export DEFAULT_BRANCH="main"
+export DEV_BRANCH="edge_${USER_IS}"
+
