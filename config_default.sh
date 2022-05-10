113d9c8496919530a223165f55e0a34b011aa498
Pascal Andy <pascalandy@users.noreply.github.com>
2022-05-10 14:38:43 -0400

    çUX: Improve how we manage CONFIGS, vars, sane defaults, idempotent, logic overrides

--- config_default.sh
+++ config_default.sh
@@ -1,15 +1,39 @@
-# wip brainstrom
+#!/usr/bin/env bash
 
-export APP_NAME="app_no_name"
-export APP_VERSION="0.1.0"
+# config_default.sh, required by bashlava
 
-export USING_PRIVATE_SCRIPTS="false"
+### You should overide these via config_default.sh
+APP_NAME="app_no_name"
+APP_VERSION="0.1.0"
+GITHUB_USER="mira"
+DOCKERHUB_USER="mira"
+GITHUB_REGISTRY="mira"
 
-export USER_IS="${USER}"
+### SANE DEFAULTS
+DEFAULT_BRANCH="main"
+USER_IS="${USER}"
+DEFAULT_DEV_BRANCH="edge_${USER_IS}"
 
 
-export DEFAULT_BRANCH="main"
-export DEV_BRANCH="edge_${USER_IS}"
+### SET PATHS
+
+
+### SOURCE FILES
+
+
+### FCT OPTIONS
+EDGE_EXTENTED="false" # not programmed yet.
+
+### GH CLI OPTION
+# i like to bypass some prompts the cli is asking
 
 # CMD CONFIGS
-export LOG__LINE_NUMBERS="8" #default is 8
+LOG__LINE_NBR_SHORT="4"
+LOG__LINE_NBR_LONG="12"
+
+# Override default values
+USE_CONFIG_CUSTOM="false"
+
+  # set only if it has no value currently
+  ${LOG__LINE_NBR_VERY_LONG:=30}
+
