0b5704c94b6b7e78b6875e2ca3be0fbf73c37eb0
Pascal Andy <pascalandy@users.noreply.github.com>
2022-05-19 16:37:06 -0400

    Update bashlava to version 1.49.1

--- .bl_env.sh
+++ .bl_env.sh
@@ -4,7 +4,7 @@
 ### Because of these three vars, you must create a use `.bl_override.sh`
 APP_NAME="bashlava"
 GITHUB_USER="firepress-org"
-APP_VERSION="1.49.0"
+APP_VERSION="1.49.1"
 
 ### DEFAULT CONFIGS BELOW
 
