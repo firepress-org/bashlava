9b4cc803a871877395ab977d8d31da82224d0ad8
Pascal Andy <pascalandy@users.noreply.github.com>
2023-11-28 14:27:42 -0500

    alias : add d for dummy

--- components/alias.sh
+++ components/alias.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 
-m() { "mainbranch" ; }
-e() { "edge" ; }
-c() { "commit" ; }
-h() { "help" ; }
-1() { "help" ; }
-te() { "test" ; }
-tt() { "test" ; }
-2() { "test" ; }
-v() { "version" ; }
-t() { "tag" ; }
-r() { "release" ; }
-om() { "git checkout ${default_branch}" ; }
-oe() { "git checkout edge" ; }
-l() { "log" ; }
-sq() { "squash" ; }
-s() { "show" ; }
-sv() { "Show_Version" ; }
-cs() { "commit_suggestion" ; }
-hash() { "git rev-parse HEAD && git rev-parse --short HEAD" ; }
-Print_Yellow() { "Print_Warning" ; }
+m() { "mainbranch"; }
+e() { "edge"; }
+c() { "commit"; }
+h() { "help"; }
+1() { "help"; }
+te() { "test"; }
+tt() { "test"; }
+2() { "test"; }
+v() { "version"; }
+t() { "tag"; }
+r() { "release"; }
+d() { "dummy"; }
+om() { "git checkout ${default_branch}"; }
+oe() { "git checkout edge"; }
+l() { "log"; }
+sq() { "squash"; }
+s() { "show"; }
+sv() { "Show_Version"; }
+cs() { "commit_suggestion"; }
+hash() { "git rev-parse HEAD && git rev-parse --short HEAD"; }
+Print_Yellow() { "Print_Warning"; }
 
-Utility_Invalid_Cmd() { "Utility_Invalid_Cmd" ; }
-sh() { "Utility_Invalid_Cmd" ; }
-00() { "Utility_Invalid_Cmd" ; }
+Utility_Invalid_Cmd() { "Utility_Invalid_Cmd"; }
+sh() { "Utility_Invalid_Cmd"; }
+00() { "Utility_Invalid_Cmd"; }
